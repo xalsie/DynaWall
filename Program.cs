9b470626cdf8a6c0a10f1d860eff5e135d882458
xalsie <xalsie.ff@hotmail.fr>
2020-10-08 15:57:43 +0200

    V1 C#

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace change_Wallpaper2
+{
+    class Program
+    {
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+
+        private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+        
+        static void Main(string[] args)
+        {
+            GetBackgroud();
+        }
+
+        public static void GetBackgroud()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://source.unsplash.com/random/1920x1080?hd,wallpapers"))
+                {
+                    var response = httpClient.SendAsync(request).Result;
+
+                    Console.WriteLine(response.RequestMessage.RequestUri.AbsoluteUri);
+
+                    Random rnd = new Random();
+
+                    string localFilename = @"C:\Users\xalsi\AppData\Local\Temp\DynaWall\" + rnd.Next(1000, 9999) + ".bmp";
+                    using (WebClient myWebClient = new WebClient())
+                    {
+                        try
+                        {
+                            string[] BmpList = Directory.GetFiles(@"C:\Users\xalsi\AppData\Local\Temp\DynaWall", "*.bmp");
+
+                            // Delete source files that were copied.
+                            foreach (string i in BmpList)
+                            {
+                                File.Delete(i);
+                            }
+
+                        }
+                        catch (DirectoryNotFoundException dirNotFound)
+                        {
+                            Console.WriteLine(dirNotFound.Message);
+                        }
+                        
+                        myWebClient.DownloadFile(response.RequestMessage.RequestUri.AbsoluteUri, localFilename);
+
+                        RefreshWallpaper(localFilename);
+                    }
+                }
+            }
+        }
+
+        public static void RefreshWallpaper(string path)
+        {
+            SystemParametersInfo( 20, 0, path, 0x01 | 0x02 );
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # DynaWall
-Change your wallpaper when unlocking
+
+Description : Change your wallpaper when unlocking.
+
+## Version 1.0.9.8 (8 octo.2020)
+    - [x] First full version in C#
+
+## Version 0.1 (1 octo.2020)
+    - [x] Download random images from Unsplash
+    - [X] First version in autoit (.ua3)
+
+### known bug
+    - [x] Bugs for refresh desktop wallpaper (fix)
